ed537f3d86393cdd06589ab5f95b2581c63f2382
dk3606 <dk3606@nyu.edu>
2019-09-17 14:49:05 -0400

    seven

--- hw1_Khosla_Shin_Zhou/scripts/question_four.R
+++ hw1_Khosla_Shin_Zhou/scripts/question_four.R
@@ -5,4 +5,6 @@ trips <- readr::read_csv("data_hw1/question_three_trips.csv")
 
 taxi_data <- inner_join(trips,fares,by = c("pickup_datetime","medallion"))
 
+dups <- taxi_data %>% count(medallion, hack_license.x, pickup_datetime) %>% filter(n>1)
+
 write.csv(taxi_data, file = "data_hw1/question_four.csv")
