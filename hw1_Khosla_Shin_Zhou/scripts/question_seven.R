43cab4c455fd221f9e84f52bba60fb9e89c0af19
dk3606 <dk3606@nyu.edu>
2019-09-17 16:00:52 -0400

    final seven

--- hw1_Khosla_Shin_Zhou/scripts/question_seven.R
+++ hw1_Khosla_Shin_Zhou/scripts/question_seven.R
@@ -4,29 +4,31 @@ library(lubridate)
 
 taxi_data <- read_csv("data_hw1/joined_taxi_data.csv")
 taxi_data <- taxi_data %>% mutate(amount = tip_amount + fare_amount)
+
+taxi_data$pickup_datetime <- as.POSIXct(taxi_data$pickup_datetime, tz = "America/New_York")
+taxi_data$dropoff_datetime <- as.POSIXct(taxi_data$dropoff_datetime, tz = "America/New_York")
+
 taxi_data <- filter(taxi_data, format(taxi_data$pickup_datetime,'%d') == "15")
-taxi_data <- filter(taxi_data, format(taxi_data$dropoff_datetime,'%d') == "15")
-taxi_data$pickup_datetime <- as.POSIXct(taxi_data$pickup_datetime, tz = "UTC")
-taxi_data$dropoff_datetime <- as.POSIXct(taxi_data$dropoff_datetime, tz = "UTC")
 
 taxi_data2 <- crossing(taxi_data,hour = c(0:23))
 
 test2a <- filter(taxi_data, hour(taxi_data$pickup_datetime) != hour(taxi_data$dropoff_datetime))
 test2 <- filter(taxi_data2, hour(taxi_data2$pickup_datetime) != hour(taxi_data2$dropoff_datetime))
-test3 <- test2[1:24,]
-test4 <- filter(test2, test2$trip_time_in_secs == max(test2$trip_time_in_secs))
 
-train3 <- test3 %>% 
-  group_by(medallion,hack_license,pickup_datetime,dropoff_datetime,hour) %>%
+final <- taxi_data2 %>% mutate(secondsPU = 60*minute(pickup_datetime)+second(pickup_datetime),secondsDO = 60*minute(dropoff_datetime)+second(dropoff_datetime))
+
+final <- final %>% mutate(seconds = as.numeric(difftime(dropoff_datetime,pickup_datetime,units = "secs")))
+
+final <- final %>% 
   mutate(proportion = case_when(hour < hour(pickup_datetime) ~ 0,
                                 hour > hour(dropoff_datetime) ~ 0,
-                                hour > hour(pickup_datetime) & hour < hour(dropoff_datetime) ~ (3600/trip_time_in_secs),
+                                hour > hour(pickup_datetime) & hour < hour(dropoff_datetime) ~ (3600/seconds),
                                 hour == hour(pickup_datetime) & hour == hour(dropoff_datetime) ~ 1,
-                                hour == hour(pickup_datetime) & hour < hour(dropoff_datetime) ~ as.numeric(difftime(ceiling_date(pickup_datetime,unit = "h"),pickup_datetime,unit = "secs"))/trip_time_in_secs, 
-                                hour == hour(dropoff_datetime) & hour > hour(pickup_datetime) ~ as.numeric(difftime(dropoff_datetime,floor_date(dropoff_datetime,unit = "h")),units = "secs")/trip_time_in_secs)) %>% 
+                                hour == hour(pickup_datetime) & hour < hour(dropoff_datetime) ~ (3600-secondsPU)/seconds, 
+                                hour == hour(dropoff_datetime) & hour > hour(pickup_datetime) ~ secondsDO/seconds)) %>% 
   group_by(hack_license,hour) %>%
   summarise(total_time_with_passengers = sum(proportion*trip_time_in_secs),
             miles_with_passengers = sum(proportion*trip_distance),
-            earnings = sum(proportion*amount))
+            amount = sum(proportion*amount))
 
-write.csv(question_six, file = "data_hw1/question_seven.csv")
+write.csv(final, file = "data_hw1/question_seven.csv")
